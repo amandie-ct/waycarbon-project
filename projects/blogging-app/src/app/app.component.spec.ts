ea4398ca5c2c2dbf2d857df80aa3a0845f769769
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2024-02-22 14:19:23 -0300

    connecting library to application

--- projects/blogging-app/src/app/app.component.spec.ts
+++ projects/blogging-app/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'blogging' title`, () => {
+  it(`should have the 'blogging-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('blogging');
+    expect(app.title).toEqual('blogging-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, blogging');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, blogging-app');
   });
 });
