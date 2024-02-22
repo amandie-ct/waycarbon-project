8ed861528b7d918dd23776d90650c91414a6e20d
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2024-02-22 19:05:36 -0300

    feat: adding styles to post

--- projects/blogging-library/src/public-api.ts
+++ projects/blogging-library/src/public-api.ts
@@ -1,7 +1,1 @@
-/*
- * Public API Surface of blogging-library
- */
-
-export * from './lib/blogging-library.service';
-export * from './lib/blogging-library.component';
 export * from './lib/components';
