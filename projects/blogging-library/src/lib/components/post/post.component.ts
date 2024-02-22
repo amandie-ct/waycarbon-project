d44882811c041a2ff19e01d54e9e806c5f89de3d
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2024-02-22 14:34:58 -0300

    feat: adding component post

---
+++ projects/blogging-library/src/lib/components/post/post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './post.component.html',
+  styleUrl: './post.component.css'
+})
+export class PostComponent {
+
+}
