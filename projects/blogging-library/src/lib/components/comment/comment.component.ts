897d4b88110cbebdf5ba633974af9a73051aa34e
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2024-02-23 12:09:45 -0300

    feat: adding font awesome package and action buttons

--- projects/blogging-library/src/lib/components/comment/comment.component.ts
+++ projects/blogging-library/src/lib/components/comment/comment.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faRetweet } from '@fortawesome/free-solid-svg-icons';
+import { faFlag } from '@fortawesome/free-regular-svg-icons';
 @Component({
   selector: 'lib-comment',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.css'
 })
@@ -13,4 +15,6 @@ export class CommentComponent {
   date = '19 de fev 2023';
   time = '8h21'
   commentText = ' O empenho em analisar a consolidação das estruturas oferece uma interessante oportunidade para verificação do retorno esperado a longo prazo. Por outro lado, o julgamento imparcial das eventualidades facilita a criação dos modos de operação convencionais.'
+  faRetweet = faRetweet;
+  faFlag = faFlag;
 }
