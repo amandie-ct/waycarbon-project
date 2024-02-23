62fc5f484a9e9fa052cc4ef26e08d913dd5d4ecb
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2024-02-22 22:19:14 -0300

    refactor: changing some components location inside the projects

--- projects/blogging-app/src/app/app.component.ts
+++ projects/blogging-app/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { PostComponent } from '../../../blogging-library/src/lib/components/post
 import { PosterComponent } from '../../../blogging-library/src/lib/components/poster/poster.component';
 import { ReplyComponent } from '../../../blogging-library/src/lib/components/reply/reply.component';
 import { ModalComponent } from '../../../blogging-library/src/lib/components/modal/modal.component';
-import { HeaderComponent } from '../../../blogging-library/src/public-api';
+import { CommentComponent } from '../../../blogging-library/src/lib/components/comment/comment.component';
+import { HeaderComponent } from '../components/header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, PostComponent, PosterComponent, ReplyComponent, ModalComponent],
+  imports: [RouterOutlet, HeaderComponent, PostComponent, PosterComponent, ReplyComponent, CommentComponent, ModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
