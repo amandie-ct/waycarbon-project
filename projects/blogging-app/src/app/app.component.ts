eac5654240a5b216d8488aa2e846c7e2e80c18d2
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2024-02-22 23:59:47 -0300

    refactoring post and poster components

--- projects/blogging-app/src/app/app.component.ts
+++ projects/blogging-app/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostComponent } from '../../../blogging-library/src/lib/components/post/post.component';
-import { PosterComponent } from '../../../blogging-library/src/lib/components/poster/poster.component';
 import { ReplyComponent } from '../../../blogging-library/src/lib/components/reply/reply.component';
 import { ModalComponent } from '../../../blogging-library/src/lib/components/modal/modal.component';
 import { CommentComponent } from '../../../blogging-library/src/lib/components/comment/comment.component';
@@ -9,7 +8,7 @@ import { HeaderComponent } from '../components/header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, PostComponent, PosterComponent, ReplyComponent, CommentComponent, ModalComponent],
+  imports: [RouterOutlet, HeaderComponent, PostComponent, ReplyComponent, CommentComponent, ModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
