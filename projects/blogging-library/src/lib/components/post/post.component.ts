bb8c2972af64db35c3f0994052648d0ee3b82658
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2024-02-22 22:32:47 -0300

    refactor: refactoring post component from lib

--- projects/blogging-library/src/lib/components/post/post.component.ts
+++ projects/blogging-library/src/lib/components/post/post.component.ts
@@ -11,4 +11,5 @@ export class PostComponent {
   postImage = './assets/header-image.jpg';
   postTitle = 'Estratégias para um novo paradigma globalizado';
   postSubtitle = 'Sobre o cuidado em identificar pontos críticos na complexidade';
+  postContent = 'Caros amigos, a mobilidade dos capitais internacionais desafia a capacidade de equalização das diversas correntes de pensamento. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a necessidade de renovação processual ainda não demonstrou convincentemente que vai participar na mudança das diretrizes de desenvolvimento para o futuro. O cuidado em identificar pontos críticos na complexidade dos estudos efetuados é uma das consequências das direções preferenciais no sentido do progresso. Do mesmo modo, a adoção de políticas descentralizadoras não pode mais se dissociar dos modos de operação convencionais. '
 }
