48ce50c760f9a5c98541a423c507a0dfcb52a3ea
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2024-02-26 13:03:20 -0300

    feat: adding modal component and container

---
+++ projects/blogging-library/src/lib/components/modal-container/modal-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-modal-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-container.component.html',
+  styleUrl: './modal-container.component.css'
+})
+export class ModalContainerComponent {
+
+}
