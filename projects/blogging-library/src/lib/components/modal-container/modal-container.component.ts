33a04a80602c04123c96a99ac1d2ab6abc13d9fd
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2024-02-26 17:37:07 -0300

    feat: trying to add userId service

--- projects/blogging-library/src/lib/components/modal-container/modal-container.component.ts
+++ projects/blogging-library/src/lib/components/modal-container/modal-container.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { UserService } from './services/user.service';
 import { ModalComponent } from '../modal/modal.component';
 import { IUser } from './types/user.interface';
+import { UserIdService } from './services/userid.service';
 
 @Component({
   selector: 'lib-modal-container',
@@ -16,7 +17,9 @@ export class ModalContainerComponent implements OnInit {
   @Input() userId!: number;
   userDetails!: IUser;
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private userIdService: UserIdService) {
+    this.userId = this.userIdService.getUserId();
+  }
 
   ngOnInit(): void {
     this.userService.getUser(this.userId).subscribe(user => {
--- projects/blogging-library/src/lib/components/modal/modal.component.ts
+++ projects/blogging-library/src/lib/components/modal/modal.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, registerLocaleData  } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faFlag } from '@fortawesome/free-regular-svg-icons';
 import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import { faUserMinus } from '@fortawesome/free-solid-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { IUser } from '../modal-container/types/user.interface';
-
+import localeBR from '@angular/common/locales/pt';
+import { UserIdService } from '../modal-container/services/userid.service';
+registerLocaleData(localeBR);
 
 
 @Component({
@@ -18,7 +20,6 @@ import { IUser } from '../modal-container/types/user.interface';
 })
 export class ModalComponent {
   @Input() user = {};
-
   faFlag = faFlag;
   faUserPlus = faUserPlus;
   faUserMinus = faUserMinus;
