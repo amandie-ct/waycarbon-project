d613f1af8f028f6a9c912db62cf14f2549c389e5
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2024-02-27 01:46:35 -0300

    fix: removing unused imports

--- projects/blogging-app/src/app/app.component.ts
+++ projects/blogging-app/src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { IPost, PostComponent } from '../../../blogging-library/src/lib/components/post/post.component';
+import { PostComponent } from '../../../blogging-library/src/lib/components/post/post.component';
 import { HeaderComponent } from './components/header/header.component';
-// import { PostService } from './services/posts/post.service';
 import { CommentsComponent } from './components/comments/comments.component';
 import { ModalContainerComponent } from '../../../blogging-library/src/lib/components/modal-container/modal-container.component';
 @Component({
@@ -15,7 +14,6 @@ import { ModalContainerComponent } from '../../../blogging-library/src/lib/compo
     CommentsComponent,
     ModalContainerComponent
   ],
-  // providers: [PostService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -24,33 +22,4 @@ export class AppComponent  {
   title = 'blogging-app';
   openModal: boolean = false;
 
-  public post: IPost = {
-    id: 0,
-    timestamp: '',
-    author: {
-      id: 1,
-      username: '',
-      avatar: ''
-    },
-    title: '',
-    subtitle: '',
-    content: '',
-    comments: [{
-      id: 1,
-      respondsTo: null,
-      author: {
-        id: 1,
-        username: ''
-      },
-      timestamp: '',
-      content: ''
-    }]
-  }
-
-
-  // constructor(private _postService: PostService) {}
-
-  // ngOnInit() {
-  //   this.post = this._postService.getPost();
-  // }
 }
